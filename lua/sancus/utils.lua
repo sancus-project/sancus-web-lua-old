2e31e685ff92d85c6499233b94a73360d9bd44ba
Alejandro Mery <amery@geeks.cl>
2011-05-17 00:07:41 +0200

    sancus.utils: added sibling_modules() helper to detect plugins

--- lua/sancus/utils.lua
+++ lua/sancus/utils.lua
@@ -3,6 +3,8 @@
 local pairs, string, type, tostring = pairs, string, type, tostring
 local debug, next = debug, next
 
+local lfs = assert(require("lfs"))
+
 module (...)
 
 -- table.show() stolen from
@@ -62,7 +64,7 @@ function pprint(t, name, indent)
 					local fname = string.format("%s[%s]", name, k)
 					field = string.format("[%s]", k)
 					-- three spaces between levels
-						addtocart(v, fname, indent .. "   ", saved, field)
+					addtocart(v, fname, indent .. "   ", saved, field)
 				end
 				cart = cart .. indent .. "};\n"
 			end
@@ -76,4 +78,22 @@ function pprint(t, name, indent)
 	cart, autoref = "", ""
 	addtocart(t, name, indent)
 	return cart .. autoref
+end
+
+function sibling_modules()
+	local source = debug.getinfo(2).source
+	local basedir, me = source:match("^@(.*[/\\])([^/\\]+)$")
+	local t = {}
+
+	if basedir then
+		for f in lfs.dir(basedir) do
+			if f ~= me then
+				local f, e = f:match("^(.*)%.([^.]+)$")
+				if e == "lua" or e == "so" or e == "dll" then
+					t[#t+1] = f
+				end
+			end
+		end
+	end
+	return t
 end
