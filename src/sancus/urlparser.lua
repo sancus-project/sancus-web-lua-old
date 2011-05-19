8cedc3a55755ab34a399ab2a3f29a59a55ac8711
Alejandro Mery <amery@geeks.cl>
2011-05-19 20:25:58 +0200

    sancus.urlparser: made parser catch all relevant events

--- src/sancus/urlparser.lua
+++ src/sancus/urlparser.lua
@@ -12,6 +12,8 @@ local pp = function (...) print(sancus.utils.pprint(...)) end
 module (...)
 
 local function parser()
+	local h, stack = {}, {}
+
 	-- Lexical Elements
 	local alpha = R("az","AZ")
 	local num = R"09"
@@ -21,6 +23,7 @@ local function parser()
 	local any = segment + P"/"
 
 	local eol, eos = P"$", P(-1)
+	local bo, eo = P"[", P"]"
 
 	-- Grammar
 	local URL, EOL = V"URL", V"EOL"
@@ -28,25 +31,33 @@ local function parser()
 	local Predicate, Name, Option = V"Predicate", V"Name", V"Option"
 	local Optional = V"Optional"
 
-	local function token(t)
-		return function(s)
-			if s == "" then
-				s = nil
-			end
-			pp(s, t)
-		end
-	end
+	-- callbacks
+	function h.literal(s) pp(s, "literal") end
+
+	function h.predicate(s) pp(s, "predicate") end
+	function h.name(s) pp(s, "name") end
+	function h.option(s) pp(s, "option") end
+
+	function h.begin_optional(s) pp(s, "begin optional") end
+	function h.end_optional(s) pp(s, "end optional") end
+
+	function h.eol(s) pp(s, "eol") end
 
 	return P{URL,
-		URL = Data * EOL,
-		Data = (Optional + Predicate + any)^1,
+		URL = Data^1 * EOL,
+		Data = Optional
+			+ Predicate/h.predicate
+			+ any/h.literal,
 
 		-- Optional <- "[" data "]"
-		Optional = (P"[" * Data * P"]")/token("optional"),
+		Optional = bo/h.begin_optional
+			* Data^1
+			* eo/h.end_optional,
 
 		-- Predicate <- "{" name (":" option ("|" option)*) "}"
-		Name = identifier/token("name"),
-		Option = (segment^1)/token("option"),
+		Name = identifier/h.name,
+		Option = (segment^1)/h.option,
+
 		Predicate = P"{" * Name * (
 			P":" * Option * (
 				P"|" * Option
@@ -54,7 +65,7 @@ local function parser()
 			)^-1 * P"}",
 
 		-- $<eos> or <eos>
-		EOL = (eol * eos)/token("eol") + eos,
+		EOL = (eol * eos)/h.eol + eos,
 	}
 end
 parser = assert(parser())
