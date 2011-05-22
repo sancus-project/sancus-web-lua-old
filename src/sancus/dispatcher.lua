b1f4e70da728fbd65331725a1a05a4854b566ed8
Alejandro Mery <amery@geeks.cl>
2011-05-22 21:32:52 +0200

    sancus.dispatcher: match the PATH_INFO against the patterns

--- src/sancus/dispatcher.lua
+++ src/sancus/dispatcher.lua
@@ -4,8 +4,9 @@ require "sancus.object"
 require "sancus.urlparser"
 require "sancus.utils"
 
-local coroutine = coroutine
-local pprint = sancus.utils.pprint
+local coroutine, pairs, assert = coroutine, pairs, assert
+local pprint = sancus.utils.pformat
+local format = string.format
 local Class = sancus.object.Class
 local TemplateCompiler = sancus.urlparser.TemplateCompiler
 
@@ -19,6 +20,7 @@ end
 
 function M:add(template, handler)
 	expr = self.compile(template)
+	assert(expr ~= nil, format("invalid template: %s", template))
 	return self:add_regex(expr, handler)
 end
 
@@ -27,7 +29,16 @@ function M:make_app()
 		local headers = { ["Content-Type"] = "text/plain" }
 
 		local function env_dump()
+			local path_info = wsapi_env.headers["PATH_INFO"] or ""
+
 			coroutine.yield(pprint(self, 'self'))
+			for regex, handler in pairs(self.patterns) do
+				local c, p = regex:match(path_info)
+				if p then
+					coroutine.yield(pprint(c, handler))
+					break
+				end
+			end
 			coroutine.yield(pprint(wsapi_env.headers, 'env'))
 		end
 
