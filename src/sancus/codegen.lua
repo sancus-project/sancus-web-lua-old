eccb754a0f127eef63d9cc0b003919ce7257d6ec
Alejandro Mery <amery@geeks.cl>
2011-05-24 18:04:42 +0200

    sancus.codegen: helper to load a templates dir into a Lua-CodeGen structure

---
+++ src/sancus/codegen.lua
@@ -0,0 +1,36 @@
+--
+
+require "CodeGen"
+require "lfs"
+
+local lfs, CodeGen = lfs, CodeGen
+local sformat, fopen = string.format, io.open
+local assert = assert
+
+module(...)
+
+function loaddir_raw(dir, out, prefix)
+	prefix = prefix or ""
+	out = out or {}
+
+	for fn in lfs.dir(dir) do
+		if not fn:match("^[.]") then
+			local ffn = sformat("%s/%s", dir, fn)
+			if lfs.attributes(ffn, "mode") == "directory" then
+				loaddir_raw(ffn, out, sformat("%s%s_", prefix, fn))
+			else
+				local bn = fn:match("^(.*)[.]([^.]+)$") or fn
+				local f = assert(fopen(ffn, "r"))
+
+				out[prefix..bn] = f:read("*all")
+				f:close()
+			end
+		end
+	end
+
+	return out
+end
+
+function loaddir(dir)
+	return CodeGen(loaddir_raw(dir))
+end
