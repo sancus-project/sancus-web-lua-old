b7d43b1f1d38ddc77313a8d7aa1d79993217e569
Alejandro Mery <amery@geeks.cl>
2011-05-28 14:31:42 +0200

    sancus.object: changed Class.__call to pass self along

--- src/sancus/object.lua
+++ src/sancus/object.lua
@@ -5,14 +5,14 @@ local setmetatable = setmetatable
 module(...)
 
 local _class = {
-	__call = function (c, ...) return c.new(...) end,
+	__call = function (c, ...) return c:new(...) end,
 }
 
 function Class(c)
 	c = c or {}
 	c.__index = c
 
-	function c.new(o)
+	function c:new(o)
 		o = o or {}
 		setmetatable(o, c)
 		return o
