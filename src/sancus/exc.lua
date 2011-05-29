bb8e2c6ad7f2fe22c4f5cd16162d8e7c2827a432
Alejandro Mery <amery@geeks.cl>
2011-05-29 12:04:52 +0200

    sancus.exc: export plain_handler_generator()

--- src/sancus/exc.lua
+++ src/sancus/exc.lua
@@ -5,12 +5,14 @@ local coroutine = coroutine
 
 module (...)
 
-local function generate_plain_handler(status, text)
-	local headers = { ["Content-Type"] = "text/plain" }
+function plain_handler_generator(status, text, headers)
 	local f = function () coroutine.yield(text) end
+
+	headers = headers or {}
+	headers["Content-Type"] = "text/plain; charset=UTF-8"
 
 	-- we cannot resume dead coroutine, so wrap f each time
 	return function(wsapi_env) return status, headers, coroutine.wrap(f) end
 end
 
-handle404 = generate_plain_handler(404, "Not Found")
+handle404 = plain_handler_generator(404, "Not Found")
